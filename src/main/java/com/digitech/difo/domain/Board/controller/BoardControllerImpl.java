c9843c54259839f028efb9b280715ebd3ef7f076
unknown <yjs12180825@gmail.com>
2024-01-09 22:45:59 +0900

    이정욱 똥 치우기 :hammer:

---
+++ src/main/java/com/digitech/difo/domain/Board/controller/BoardControllerImpl.java
@@ -0,0 +1,49 @@
+package com.digitech.difo.domain.Board.controller;
+
+import com.digitech.difo.global.common.SuccessResponse;
+import jakarta.websocket.server.PathParam;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.digitech.difo.domain.Board.service.BoardService;
+import com.digitech.difo.domain.Board.domain.Board;
+
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class BoardControllerImpl implements BoardController {
+    /**
+     * 이정욱 시봉봉봉방방방놈아
+     */
+    private final BoardService boardService;
+
+    @Override
+    public ResponseEntity<SuccessResponse<List<Board>>> getEntriesByPage(@PathVariable int index, @PathParam(value = "count") int pageCount) {
+        SuccessResponse<List<Board>> boards = boardService.getListsByPage(index, pageCount);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+
+        return new ResponseEntity<>(boards, headers, HttpStatus.OK);
+    }
+
+    @Override
+    public Board getPostById(@PathVariable Long id) {
+        return boardService.getPostById(id);
+    }
+
+    @PostMapping("/register")
+    public void createBoard(){
+        return;
+    }
+
+    @PostMapping("/posts/{id}/like")
+    public void addLikes(@PathVariable Long id, int likes){
+        return;
+    }
+}
