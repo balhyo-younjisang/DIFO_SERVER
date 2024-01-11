6394b62046b873beb93b792b920af19295369ec2
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2024-01-11 13:37:21 +0900

    :sparkles: 댓글 조회 기능 01-11 -> 이정욱

--- src/main/java/com/digitech/difo/domain/BoardComment/controller/BoardCommentControllerImpl.java
+++ src/main/java/com/digitech/difo/domain/BoardComment/controller/BoardCommentControllerImpl.java
@@ -1,7 +1,6 @@
 package com.digitech.difo.domain.BoardComment.controller;
 
 import com.digitech.difo.domain.Board.domain.Board;
-import com.digitech.difo.domain.Board.service.BoardService;
 import com.digitech.difo.domain.BoardComment.domain.BoardComment;
 import com.digitech.difo.domain.BoardComment.service.BoardCommentService;
 import com.digitech.difo.global.common.SuccessResponse;
@@ -17,13 +16,13 @@ import java.util.Optional;
 public class BoardCommentControllerImpl implements BoardCommentController {
     private BoardCommentService boardCommentService;
     @Override
-    public ResponseEntity<SuccessResponse<List<Board>>> getComments(long id) {
-        return null;
+    public ResponseEntity<SuccessResponse<List<BoardComment>>> getComments(long id) {
+        return boardCommentService.getComments(id);
     }
 
     @Override
-    public void createComment(BoardComment boardComment, Optional<Long> memberId, long id) throws Exception {
-        boardCommentService.createBoardComment(boardComment, memberId, id);
+    public void createComment(BoardComment boardComment, long id, Optional<Long> memberId) throws Exception {
+        boardCommentService.createBoardComment(boardComment, id, memberId);
     }
 
 
--- src/main/java/com/digitech/difo/domain/BoardComment/repository/BoardCommentRepository.java
+++ src/main/java/com/digitech/difo/domain/BoardComment/repository/BoardCommentRepository.java
@@ -3,6 +3,10 @@ package com.digitech.difo.domain.BoardComment.repository;
 import com.digitech.difo.domain.Board.domain.Board;
 import com.digitech.difo.domain.BoardComment.domain.BoardComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
+    List<BoardComment> findAllCommentsByBoardBoardId(long boardId);
 }
